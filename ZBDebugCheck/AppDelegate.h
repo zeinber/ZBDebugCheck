319728aee3c64919f55e7988ac785bb6227c9f87
zeinber <zhanbin.cheng@tongdun.net>
2020-12-19 16:45:43 +0800

    feat: 项目结构调整，初始化

---
+++ ZBDebugCheck/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZBDebugCheck
+//
+//  Created by 隐姓埋名 on 2020/12/19.
+//  Copyright © 2020 展斌程. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
