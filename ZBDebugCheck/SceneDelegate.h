f8e271c3fbe3bb455ff79046452c5824b9eed510
zeinber <zhanbin.cheng@tongdun.net>
2020-12-19 16:22:15 +0800

    fix: 修改.gitignore文件，忽略.DS_Store

---
+++ ZBDebugCheck/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ZBDebugCheck
+//
+//  Created by 隐姓埋名 on 2020/12/19.
+//  Copyright © 2020 展斌程. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ ZBDebugCheck/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ZBDebugCheck
+//
+//  Created by 隐姓埋名 on 2020/12/19.
+//  Copyright © 2020 展斌程. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
